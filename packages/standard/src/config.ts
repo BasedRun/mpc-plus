35cc9bc446b11e7a67452258a7c285f142dbdb72
pan.kechun <pan.kechun@danatech.cn>
2026-08-31 18:55:21 +0800

    feat(config): support environment-specific platform settings

--- packages/standard/src/config.ts
+++ packages/standard/src/config.ts
@@ -1,20 +1,42 @@
-import type { WechatConfig } from '@mpc-plus/wechat'
+import type {WechatConfig} from '@mpc-plus/wechat'
+import type {BaseMpcConfig} from "@mpc-plus/core";
 
-export interface MPCConfig {
-  project?: {
-    root?: string
-  }
-
-  release?: {
-    version?: string
-    description?: string
-  }
-
-  platforms?: {
-    wechat?: WechatConfig[]
-  }
+export interface StandardPlatformsConfig {
+  wechat?: WechatConfig[]
 }
+
+export type MPCConfig = BaseMpcConfig<StandardPlatformsConfig>
 
 export function defineConfig(config: MPCConfig): MPCConfig {
   return config
+}
+
+export function resolvePlatformConfig<K extends keyof StandardPlatformsConfig>(
+    config: MPCConfig,
+    platform: K,
+    env: string
+) {
+  const platformConfigs = config.platforms?.[platform]
+
+  if (!platformConfigs) {
+    throw new Error(`Platform ${platform} is not configured.`)
+  }
+
+  const  envConfig = platformConfigs.find(e => e.env === env)
+  if (!envConfig) {
+    throw new Error(`Environment ${platform} is not configured for ${platform}`)
+  }
+
+  return {
+    ...envConfig,
+    project: {
+      ...config.project,
+      ...envConfig.project,
+    },
+
+    release: {
+      ...config.release,
+      ...envConfig.release,
+    }
+  }
 }
--- packages/wechat/src/config.ts
+++ packages/wechat/src/config.ts
@@ -1,6 +1,9 @@
+import type {ProjectConfig, ReleaseConfig} from "@mpc-plus/core";
+
 export interface WechatConfig{
   env: string;
   appid: string;
   privateKey: string;
-  projectRoot?: string;
+  project?: ProjectConfig;
+  release?: ReleaseConfig;
 }
